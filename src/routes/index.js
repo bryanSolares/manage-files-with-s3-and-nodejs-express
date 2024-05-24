7262028aa08daf7666387d4b5a2f5eb6d4f5f377
BRYAN JOSUE SOLARES PEREZ <bryan.solares@claro.com.gt>
2024-05-24 08:53:57 -0600

    docs: :memo: Adding final documentation for endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -46,7 +46,7 @@ routes.get('/', (_, res) => {
  *  name: Manage files
  *  description: Manage files into S3 Bucket
  * paths:
- *  /api/v1/files/upload:
+ *  /api/v1/files/upload/:
  *      post:
  *          summary: Upload file into S3 Bucket
  *          tags: [Manage files]
@@ -60,7 +60,7 @@ routes.get('/', (_, res) => {
  *                type: string
  *                required: true
  *          responses:
- *              200:
+ *              201:
  *                  description: "Successfully upload file"
  *                  content:
  *                      application/json:
@@ -73,8 +73,26 @@ routes.get('/', (_, res) => {
  *                                      type: string
  *                                  url:
  *                                      type: string
+ *              400:
+ *                  description: "No user or file!"
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  message:
+ *                                      type: string
+ *              500:
+ *                  description: "Internal server error!"
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  message:
+ *                                      type: string
  *
- *  /api/v1/files:
+ *  /api/v1/files/:
  *     delete:
  *      summary: Delete file into S3 Bucket
  *      tags: [Manage files]
@@ -97,28 +115,123 @@ routes.get('/', (_, res) => {
  *                          properties:
  *                              message:
  *                                  type: string
+ *          400:
+ *              description: "No user or id!"
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *          500:
+ *              description: "Internal server error!"
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
  *
  *  /api/v1/files:
  *      get:
- *       summary: Get file into S3 Bucket
- *       tags: [Manage files]
- *       parameters:
- *           - in: query
- *             name: key
- *             type: string
- *             required: true
- *       responses:
- *           200:
- *               description: "Successfully get file"
- *               content:
- *                   application/json:
- *                       schema:
- *                           type: object
- *                           properties:
- *                               message:
- *                                   type: string
- *                               url:
- *                                   type: string
+ *          summary: Get file from S3 Bucket
+ *          tags: [Manage files]
+ *          parameters:
+ *              - in: query
+ *                name: key
+ *                type: string
+ *                required: true
+ *          responses:
+ *              200:
+ *                  description: "Successfully list files"
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  message:
+ *                                      type: string
+ *                                  url:
+ *                                      type: string
+ *              400:
+ *                  description: "No key!"
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  message:
+ *                                      type: string
+ *              404:
+ *                  description: "File not found!"
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  message:
+ *                                      type: string
+ *              403:
+ *                  description: "Access denied!"
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  message:
+ *                                      type: string
+ *              500:
+ *                  description: "Internal server error!"
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  message:
+ *                                      type: string
+ *
+ *  /api/v1/files/list:
+ *      get:
+ *          summary: List files into S3 Bucket
+ *          tags: [Manage files]
+ *          parameters:
+ *              - in: query
+ *                name: user
+ *                type: string
+ *                required: true
+ *          responses:
+ *              200:
+ *                  description: "Successfully list files"
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  message:
+ *                                      type: string
+ *                                  url:
+ *                                      type: string
+ *              400:
+ *                  description: "No user!"
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  message:
+ *                                      type: string
+ *              500:
+ *                  description: "Internal server error!"
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  message:
+ *                                      type: string
+ *
  */
 routes.use('/api/v1/files', files)
 
